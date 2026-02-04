3d9a150cd704b20a4c9ec1d9c41ad5bc1cf3daff
Dolly Suman <dolly.suman.023@gmail.com>
2026-02-04 21:48:01 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,36 +1,47 @@
 import React from 'react'
-import { Form , useForm} from 'react-hook-form'
+import { Form, useForm } from 'react-hook-form'
 
 
 
 const Contact = () => {
-  
+
   const {
     register,
     handleSubmit,
     setError,
     formState: { errors, isSubmitting },
   } = useForm();
+
+  const onSubmit = async(data) =>{
+    let r = await fetch("http://localhost:5000/contact", {method: "POST", headers:{
+      "Content-Type": "application/json",
+    },body: JSON.stringify(data)})
+
+    let res = await r.text();
+    console.log(data,res);
+  }
   return (
     <div>
-        <div className="connect">
+      <div className="connect">
 
-      <div className="cont">Contact Me</div>
-      <div className="note">Have a question or want to work together? Leave your details and I'll get back to you as soon as possible.</div>
-      <div className="getemail">
-      <form action="" ></form>
-        <div className="putname">
-        <input type="text" placeholder='Name' {...register('name',{required: {value: true, message: "This field is required"}})} />
-        </div>
+        <div className="cont">Contact Me</div>
+        <div className="note">Have a question or want to work together? Leave your details and I'll get back to you as soon as possible.</div>
+        <div className="getemail">
+          <form action="" onSubmit={handleSubmit(onSubmit)}  >
 
-        <div className="putemail">
-        <input type="text" placeholder='Email' {...register('email',{required: {value: true, message: "This field is required"}})} />
-        </div>
+            <div className="putname">
+              <input type="text" placeholder='Name' {...register('name', { required: { value: true, message: "This field is required" } })} />
+            </div>
 
-        <div className="putmsg">
-        <input type="text" placeholder='Message' {...register('Message', {required: {value: true, message: "This field is required"}})} />
-        </div>
-        <input  type="submit" value="Submit" />
+            <div className="putemail">
+              <input type="text" placeholder='Email' {...register('email', { required: { value: true, message: "This field is required" } })} />
+            </div>
+
+            <div className="putmsg">
+              <input type="text" placeholder='Message' {...register('Message', { required: { value: true, message: "This field is required" } })} />
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
 
         </div>
       </div>
