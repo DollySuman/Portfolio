f2e906c153a69c38e543f85978005fc9c9cbbba3
Dolly Suman <dolly.suman.023@gmail.com>
2026-02-07 17:09:23 +0530

    make contact backend to send mails

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Form, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import Navbar from './Navbar'
+import './Contact.css'
 
 
 
@@ -12,25 +14,37 @@ const Contact = () => {
     formState: { errors, isSubmitting },
   } = useForm();
 
-  const onSubmit = async(data) =>{
-    let r = await fetch("http://localhost:5000/contact", {method: "POST", headers:{
-      "Content-Type": "application/json",
-    },body: JSON.stringify(data)})
+  // const onSubmit = async(data) =>{
+  //   let r = await fetch("http://localhost:5000/contact", {method: "POST", headers:{
+  //     "Content-Type": "application/json",
+  //   },body: JSON.stringify(data)})
 
-    let res = await r.text();
-    console.log(data,res);
+  //   let res = await r.text();
+  //   console.log(data,res);
+  // }
+
+  const onSubmit = async(data) => {
+    let r = await fetch("http://localhost:5000/contact",{method: "POST", header:{
+      "Content-Type": "application/json",
+    }, body: JSON.stringify(data)
+  })
+
+  let res = await r.text();
+  console.log(data,res);
   }
   return (
     <div>
+      <Navbar/>
       <div className="connect">
 
         <div className="cont">Contact Me</div>
         <div className="note">Have a question or want to work together? Leave your details and I'll get back to you as soon as possible.</div>
         <div className="getemail">
-          <form action="" onSubmit={handleSubmit(onSubmit)}  >
+          <form className='flex' action="" onSubmit={handleSubmit(onSubmit)}  >
 
             <div className="putname">
               <input type="text" placeholder='Name' {...register('name', { required: { value: true, message: "This field is required" } })} />
+              {errors.name && <span className="error">{errors.name.message}</span>}
             </div>
 
             <div className="putemail">
