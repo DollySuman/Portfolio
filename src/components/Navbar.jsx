61273c215b995f9f0c74005864d62c23bd75264c
Dolly Suman <dolly.suman.023@gmail.com>
2026-02-15 19:39:09 +0530

    add projs pics

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <NavLink to="/home" className={(e)=>{return e.isActive?"link": "anchor"}} ><li>Home</li></NavLink>
             <NavLink to="/about" className={(e)=>{return e.isActive?"link": "anchor"}} ><li>About</li></NavLink>
             <NavLink to="/contact" className={(e)=>{return e.isActive?"link": "anchor"}} ><li>Contact</li></NavLink>
-          <a href="https://github.com/DollySuman" target='_blank'>Github</a>
+          <a className='git' href="https://github.com/DollySuman" target='_blank'>Github</a>
             
         </ul>
       </nav>
